8117436ced7ac182838454a37ffceaee6d9e6b53
Mohammad Ghabin <v-mghabin@microsoft.com>
2022-12-04 14:27:04 +0200

    revert changes

--- data/repositories.data.ts
+++
@@ -1,5 +0,0 @@
-export const repositoriesCommon = {
-    repositoryName1 : 'test1',
-    repositoryDescription1 : 'create a new repo for testing purpose',
-    repositoryName2 : 'test2',
-};
--- playwright.config.ts
+++ playwright.config.ts
@@ -13,7 +13,7 @@ import { devices } from '@playwright/test';
 const config: PlaywrightTestConfig = {
   testDir: './tests',
   /* Maximum time one test can run for. */
-  timeout: 20 * 1000,
+  timeout: 30 * 1000,
   expect: {
     /**
      * Maximum time expect() should wait for the condition to be met.
@@ -31,7 +31,6 @@ const config: PlaywrightTestConfig = {
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: 'html',
-
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Maximum time each action such as `click()` can take. Defaults to 0 (no limit). */
@@ -51,7 +50,7 @@ const config: PlaywrightTestConfig = {
         ...devices['Desktop Chrome'],
       },
     },
-    /*
+
     {
       name: 'firefox',
       use: {
@@ -65,7 +64,7 @@ const config: PlaywrightTestConfig = {
         ...devices['Desktop Safari'],
       },
     },
-    */
+
     /* Test against mobile viewports. */
     // {
     //   name: 'Mobile Chrome',
