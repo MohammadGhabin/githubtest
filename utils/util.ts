034e8bec8be46cb930bed71aa90854a073749204
Mohammad Ghabin <v-mghabin@microsoft.com>
2022-12-04 14:50:54 +0200

    add util file

---
+++ utils/util.ts
@@ -0,0 +1,18 @@
+import {Locator, Page} from "@playwright/test"
+
+export class Util {
+    page: Page;
+
+    constructor(page: Page){
+        this.page = page;
+    }
+
+    async goto(url: string): Promise<void>{
+        await this.page.goto(url);
+    }
+
+    async click(locator: Locator, selector: string, nextState: "attached" | "detached" | "visible" | "hidden" | undefined){
+        await locator.click();
+        await this.page.waitForSelector(selector, {state: nextState})
+    }
+}
